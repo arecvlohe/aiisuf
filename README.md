a2458bfcbc1483638a801415c7e5c0763fcac493
Adam Recvlohe <adam.recvlohe@gmail.com>
2017-12-22 14:04:58 -0500

    First commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# AIIS at UF
+
+A simple single page website that informs you about AIIS work at UF.
---
+++ src/App.js
@@ -0,0 +1,9 @@
+import React, { Component } from "react";
+
+class App extends Component {
+  render() {
+    return <div>AIIS at UF</div>;
+  }
+}
+
+export default App;
